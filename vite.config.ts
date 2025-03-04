f86e9019b6a2b718d80bafbc6da50c66e7047257
hyunwoo <hyunwoo@batalyse.com>
2025-03-04 11:04:43 +0100

    Define base url.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: "/",
   resolve: {
     alias: {
       vue: "vue/dist/vue.esm.js",
